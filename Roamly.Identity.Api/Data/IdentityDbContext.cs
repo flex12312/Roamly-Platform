afabb84271cc121c982565621ad802cba4c29b39
flex12312 <davidtavk06@mail.ru>
2026-02-07 17:04:17 +0300

    feat: add ApplicationUser and IdentityDbContext configuration

---
+++ Roamly.Identity.Api/Data/IdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Roamly.Identity.Api.Models;
+
+namespace Roamly.Identity.Api.Data
+{
+    public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Roamly.Identity.Api/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Roamly.Identity.Api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public DateOnly RegistrationDate { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+    }
+}
